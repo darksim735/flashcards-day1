18273d56d9164b4ac87b9f1790aa1106ee4775fe
darksim735 <darksim735@gmail.com>
2015-03-28 14:38:55 -0400

    clicking a flashcard console logs the correctness value of the card clicked

--- public/app.js
+++ public/app.js
@@ -1,7 +1,26 @@
 var app = angular.module('flashcards', []);
 
-app.controller('FlashCardController', function(){
+app.controller('FlashCardController', function($scope){    
 
+
+$scope.checkAnswer = function(thing){
+console.log("hello, function fired");
+//console.log($scope.flashCard.correct);
+console.log(thing);
+
+};
+
+    
+    $scope.flashCard = {
+        question: 'What is Angular?',
+        answers: [
+            { text: 'A front-end framework for great power!', correct: true},
+            { text: 'Greg owns a cat', correct: false },
+            { text: 'The name of Gregs pet dog', correct: false} 
+                 ]
+                        };
 
 });
+
+
 
